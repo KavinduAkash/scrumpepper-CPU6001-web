bbedd3f44746e747fb8f7fed88f8da899c80c281
KavinduAkash <bkkasamarasingha1220@gmail.com>
2022-01-21 08:55:26 +0530

    develop create corporate project uis

--- src/views/app-views/corporate/CorporateManagement/index.js
+++ src/views/app-views/corporate/CorporateManagement/index.js
@@ -1,6 +1,20 @@
 import React from "react";
-import {AutoComplete, Avatar, Button, Card, Col, Modal, Row, Table, Tooltip, Input, Tabs} from "antd";
-import { UserOutlined, AppleOutlined, AntDesignOutlined } from '@ant-design/icons';
+import {
+    AutoComplete,
+    Avatar,
+    Button,
+    Card,
+    Col,
+    Modal,
+    Row,
+    Table,
+    Tooltip,
+    Input,
+    Tabs,
+    Form,
+    Upload, Image, Select, message
+} from "antd";
+import { UserOutlined, AppleOutlined, PlusOutlined } from '@ant-design/icons';
 import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
 import * as corporate_actions from "../../../../redux/actions/Corporate";
@@ -14,6 +28,7 @@ const { Meta } = Card;
 const size = 'small';
 
 const { TabPane } = Tabs;
+const { Option } = AutoComplete;
 
 const columns = [
     {
@@ -91,6 +106,33 @@ const member_columns_employee = [
     }
 ];
 
+const create_project_columns_employee = [
+    {
+        title: 'Member',
+        dataIndex: 'name',
+        key: 'name',
+        width: '30%',
+    },
+    {
+        title: 'email',
+        dataIndex: 'email',
+        key: 'email',
+        width: '40%',
+    },
+    {
+        title: 'Role',
+        dataIndex: 'role',
+        key: 'role',
+        width: '25%',
+    },
+    {
+        title: '',
+        dataIndex: 'action',
+        key: 'action',
+        width: '5%',
+    }
+];
+
 class CorporateManagementView extends React.Component {
 
     state = {
@@ -98,7 +140,18 @@ class CorporateManagementView extends React.Component {
         corporate:null,
         access_type:null,
         employee_list:[],
-        projects:[]
+        projects:[],
+        corporate_employee_results:[],
+
+        add_corporate_employee_modal: false,
+
+        //create new project
+        create_project_modal: false,
+        temporary_selected_employee: null,
+        temporary_selected_employee_search: "",
+        temporary_selected_employee_role: 'TEAM_MEMBER',
+        temporary_selected_employees_list: [],
+        temporary_selected_employees_list_x: [],
 
     };
 
@@ -181,9 +234,91 @@ class CorporateManagementView extends React.Component {
     });
 
 
+    onSearchCorporateEmployees = value => {
+        let res = [];
+
+        if(Cookies.get('68e78905f4c')=="" ||
+            Cookies.get('68e78905f4c')==null ||
+            Cookies.get('68e78905f4c')==undefined) {
+            this.props.history.push("/auth/login");
+        }
+
+        let headers = {
+            'Content-Type':'application/json',
+            'Authorization':'Bearer ' + Cookies.get('68e78905f4c')
+        };
+
+        let req_obj = {
+            corporateId: this.props.corporateReducer.corporate_id,
+            search: value
+        };
+
+        axios.post('http://localhost:8080/v1/corporate/employee/search', req_obj, {headers})
+            .then(res => {
+                console.log(res.data);
+                this.setState({corporate_employee_results:res.data.body})
+            })
+            .catch(err => {
+                console.log(err)
+            });
+
+    };
+
+    setCreateProjectModalVisibility = value => {
+        this.setState({create_project_modal: value});
+    };
 
 
+    onSelectNewCorporateEmployeeToAdd = employee => {
+        this.setState({temporary_selected_employee: employee});
+    };
 
+    addMemberToProjectCreate = () => {
+      let temporarySelectedEmployee = this.state.temporary_selected_employee;
+      let temporarySelectedEmployeeRole = this.state.temporary_selected_employee_role;
+      let temporarySelectedEmployeesList = this.state.temporary_selected_employees_list;
+      let temporarySelectedEmployeesList_x = this.state.temporary_selected_employees_list_x;
+      let obj = {
+          member:temporarySelectedEmployee,
+          role:temporarySelectedEmployeeRole
+      }
+
+
+      if(temporarySelectedEmployeesList_x.includes(temporarySelectedEmployee)) {
+          message.info('This member already added');
+      } else {
+          temporarySelectedEmployeesList.push(obj);
+          temporarySelectedEmployeesList_x.push(temporarySelectedEmployee);
+          this.setState({
+              temporary_selected_employees_list: temporarySelectedEmployeesList,
+              temporary_selected_employees_list_x: temporarySelectedEmployeesList_x,
+          });
+          this.clearProjectTempUserAddFields();
+      }
+    };
+
+    handleChangeScrumRole = val => {
+        this.setState({temporary_selected_employee_role: val});
+    }
+
+    searchProjectTempUser = val => {
+        this.setState({temporary_selected_employee_search: val})
+    }
+
+    clearProjectTempUserAddFields = () => {
+        this.setState({temporary_selected_employee_search: "", temporary_selected_employee_role: "TEAM_MEMBER"})
+    }
+
+    removeTempMembersFromCreateProject = val => {
+        let temporarySelectedEmployeesList = this.state.temporary_selected_employees_list;
+        let temporarySelectedEmployeesList_x = this.state.temporary_selected_employees_list_x;
+        temporarySelectedEmployeesList.splice(val, 1);
+        temporarySelectedEmployeesList_x.splice(val, 1);
+        this.setState({
+            temporary_selected_employees_list: temporarySelectedEmployeesList,
+            temporary_selected_employees_list_x: temporarySelectedEmployeesList_x,
+        });
+    };
 
     render() {
 
@@ -236,8 +371,146 @@ class CorporateManagementView extends React.Component {
             projects.push(obj);
         });
 
+
+
+        let mapped_corporate_employee_results = [];
+        if(this.state.corporate_employee_results!=null && this.state.corporate_employee_results!="" && this.state.corporate_employee_results!=undefined) {
+            this.state.corporate_employee_results.map((val) => {
+                    mapped_corporate_employee_results.push(
+                        <Option key={val.id} value={`${val.user.firstName} ${val.user.lastName} • ${val.user.refNo}`} className={'user-search-option'}>
+                            <div className={'option-container'} onClick={()=>this.onSelectNewCorporateEmployeeToAdd(val)}>
+                                <div>
+                                    <Avatar src={<Image src="https://joeschmoe.io/api/v1/random" style={{ width: 32 }} />} />
+                                </div>
+                                <div>
+                                    <div className={'topic'}>{`${val.user.firstName} ${val.user.lastName} `}<span className={'description'}>{`• ${val.user.refNo}`}</span></div>
+                                    <div className={'description'}>{`• ${val.user.email}`}</div>
+                                </div>
+                            </div>
+                        </Option>
+                    )
+                }
+            );
+        }
+
+
+            let temporary_selected_employees_list = [];
+            this.state.temporary_selected_employees_list.map((val, index) => {
+                console.log("hhhhhhhhhhhhhhhhhhhhh: ", this.state.temporary_selected_employees_list);
+                let obj = {
+                    key: index,
+                    name: val.member.user.firstName + " " + val.member.user.lastName,
+                    email: val.member.user.email,
+                    role: val.role,
+                    action:  <div>
+                        <Button type="text" danger onClick={()=>this.removeTempMembersFromCreateProject(index)}>
+                            Remove
+                        </Button>
+                    </div>
+                }
+                temporary_selected_employees_list.push(obj);
+            });
+
+        console.log("-------------------------------");
+        console.log(mapped_corporate_employee_results);
+
+
+
+
+
         return (
          <>
+             {/*modals*/}
+             {/*create a project modal*/}
+             <Modal
+                 title={`Create a Project @ ${(this.state.corporate!=null && this.state.corporate!="" && this.state.corporate!=undefined)?this.state.corporate.name:null}`}
+                 centered
+                 // visible={this.state.visible}
+                 visible={this.state.create_project_modal}
+                 onCancel={() => this.setCreateProjectModalVisibility(false)}
+                 width={1000}
+                 footer={null}
+             >
+
+                 <div>
+                     <Form layout="vertical" onFinish={this.handleSubmitCreateCorporate}>
+
+                         <Row>
+                             <Col sm={24} md={24} lg={24} xl={24}>
+                                 <Form.Item
+                                     name="project_name"
+                                     label="Project Name"
+                                     // rules={rules.corporate_name}
+                                     hasFeedback
+                                 >
+                                     <Input placeholder={'Project Name'} value={this.state.name} onChange={this.onChangeCorporateName} />
+                                 </Form.Item>
+                             </Col>
+                         </Row>
+
+                         <Row>
+                             <Col sm={24} md={16} lg={16} xl={16}>
+                                 <AutoComplete style={{ width: '95%' }} onSearch={this.onSearchCorporateEmployees} value={this.state.temporary_selected_employee_search} onChange={this.searchProjectTempUser}placeholder="input here">
+                                     {mapped_corporate_employee_results}
+                                 </AutoComplete>
+                             </Col>
+                             <Col sm={24} md={6} lg={6} xl={6}>
+                                 <Select defaultValue="TEAM_MEMBER" value={this.state.temporary_selected_employee_role} style={{ width: '95%' }}
+                                         onChange={this.handleChangeScrumRole}
+                                 >
+                                     <Option value="TEAM_MEMBER">Team Member</Option>
+                                     <Option value="SCRUM_MASTER">Scrum Master</Option>
+                                     <Option value="PRODUCT_OWNER">Product Owner</Option>
+                                     <Option value="OTHER">Other</Option>
+                                 </Select>
+                             </Col>
+                             <Col sm={24} md={2} lg={2} xl={2}>
+                                 <Button type="text" primary style={{color:'#3e79f7'}} onClick={this.addMemberToProjectCreate}>
+                                     Add
+                                 </Button>
+                             </Col>
+                         </Row>
+                         <Row>
+                             <Col sm={24} md={24} lg={24} xl={24} className={'text-center w-100 mt-5'}>
+                                 <Table dataSource={temporary_selected_employees_list} columns={create_project_columns_employee}/>
+                             </Col>
+                         </Row>
+                         <Row>
+                             <Col sm={24} md={24} lg = {24} xl={24} className={'text-right mt-3'}>
+                                 <Button
+                                     type="primary"
+                                     size={size}
+                                     className={'sp-main-btn'}
+                                     onClick={this.openCorporateCreateModal}
+                                 >
+                                     Create The Project
+                                 </Button>
+                             </Col>
+                         </Row>
+                     </Form>
+                 </div>
+
+
+
+
+
+             </Modal>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
              <div className={'page-margin-handler'}>
 
              <Tabs defaultActiveKey="1">
@@ -375,7 +648,20 @@ class CorporateManagementView extends React.Component {
                      key="3"
                  >
                      <Row>
-                         <Col sm={24} md={24} lg={24} xl={24} className={'text-center'}>
+                         <Col sm={24} md={24} lg = {24} xl={24} className={'text-right'}>
+                             <Button
+                                 type="primary"
+                                 icon={<PlusOutlined />}
+                                 size={size}
+                                 className={'sp-main-btn'}
+                                 onClick={()=>this.setCreateProjectModalVisibility(true)}
+                             >
+                                 Create a Project
+                             </Button>
+                         </Col>
+                     </Row>
+                     <Row>
+                         <Col sm={24} md={24} lg={24} xl={24} className={'text-center mt-5'}>
                              <AutoComplete
                                  dropdownClassName="certain-category-search-dropdown"
                                  dropdownMatchSelectWidth={500}
