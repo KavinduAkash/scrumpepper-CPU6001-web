90ceb11dcaae3b34a747ad7ae799c7eaeafb80b4
KavinduAkash <bkkasamarasingha1220@gmail.com>
2022-02-09 00:42:59 +0530

    develop create sprint

--- src/components/sp-componenets/sprint-components/sprint-edit-modal.js
+++ src/components/sp-componenets/sprint-components/sprint-edit-modal.js
@@ -1,19 +1,117 @@
 import React from "react";
 import {Button, Form, Input, Modal} from "antd";
-import { DatePicker, Space } from 'antd';
+import { DatePicker, message } from 'antd';
+import Cookies from "js-cookie";
+import axios from "axios";
+import * as BaseUrl from "../../../server/base_urls";
+import moment from "moment";
 const { RangePicker } = DatePicker;
+const key = 'updatable';
 
 class SprintEditModal extends React.Component {
 
+    state = {
+        name: '',
+        goal: '',
+        startDate: '',
+        endDate: '',
+    }
+
     onClose = () => {
+        this.cleanInputs();
+        this.props.onClose(null, false);
+    }
+
+    onChangeName = e => {
+        this.setState({name: e.target.value})
+    }
+
+    onChangeGoal = e => {
+        this.setState({goal: e.target.value})
+    }
+
+    onChangeDateRange = (date, dateString) => {
+        if (dateString[0] !== "" || dateString[1] !== "") {
+            let start = new Date(dateString[0]);
+            let end = new Date(dateString[1]);
+            start = (start.toISOString()).split("T")[0];
+            end = (end.toISOString()).split("T")[0];
+            this.setState({
+                startDate: start,
+                endDate: end
+            });
+        }
+    };
+
+    cleanInputs = () => {
+        this.setState({
+            name: '',
+            goal: '',
+            startDate: '',
+            endDate: '',
+        })
+    }
+
+// Create --------------------------------------------------------------------------------------------------------------
+    createSprint = () => {
+        if(Cookies.get('68e78905f4c')=="" ||
+            Cookies.get('68e78905f4c')==null ||
+            Cookies.get('68e78905f4c')==undefined) {
+            this.props.history.push("/auth/login");
+        }
+
+        let headers = {
+            'Content-Type':'application/json',
+            'Authorization':'Bearer ' + Cookies.get('68e78905f4c')
+        };
+
+        let body = {
+            projectId: this.props.projectId,
+            sprintName: this.state.name,
+            startDate: this.state.startDate,
+            endDate: this.state.endDate,
+            description: this.state.goal,
+        }
+
+        let method = "post";
+
+        axios[method](`${BaseUrl.SCRUM_PEPPER_API_URL(BaseUrl.URL_TYPE)}sprint/add`, body, {headers: headers})
+            .then(async response => {
+                if(response.status==200) {
+                    if(response.data.success) {
+                        message.success('Sprint created successfully!');
+                        this.cleanInputs();
+                        this.onClose();
+                    }
+                }
+            }).catch(async error => {
+            message.error('Something went wrong!');
+        });
+    }
+
+// Update --------------------------------------------------------------------------------------------------------------
+    updateSprint = () => {
     }
 
     render() {
+
+        // Modal function type
+        let type = this.props.type;
+
+        // Date Range
+        let dateRange = null;
+        if (this.state.startDate !== null && this.state.startDate !== undefined && this.state.startDate !== "") {
+            let startdate = this.state.startDate;
+            let enddate = this.state.endDate;
+            dateRange = [moment(startdate), moment(enddate)];
+        } else {
+            dateRange = null;
+        }
+
         return(
             <Modal
-                title="Basic Modal" v
-                // visible={this.props.isEditVisible}
-                visible={false}
+                title="Basic Modal"
+                visible={this.props.isEditVisible}
                 onCancel={this.onClose}
                 footer={false}
                 className={'sprint-edit-modal'}
@@ -28,22 +126,27 @@ class SprintEditModal extends React.Component {
                         required
                         // tooltip="This is a required field"
                     >
-                        <Input placeholder="input placeholder" />
+                        <Input placeholder="input placeholder" value={this.state.name} onChange={this.onChangeName} />
                     </Form.Item>
                     <Form.Item
                         label="Sprint Goal"
                         // tooltip={{ title: 'Tooltip with customize icon', icon: <InfoCircleOutlined /> }}
                     >
-                        <Input.TextArea placeholder="input placeholder" />
+                        <Input.TextArea placeholder="input placeholder" value={this.state.goal} onChange={this.onChangeGoal} />
                     </Form.Item>
                     <Form.Item
                         label="Sprint Duration"
                         // tooltip="This is a required field"
                     >
-                        <RangePicker />
+                        <RangePicker value={dateRange} onChange={this.onChangeDateRange} />
                     </Form.Item>
                     <Form.Item className={'text-right'}>
-                        <Button type="primary">Submit</Button>
+                        {
+                            type=='CREATE'?
+                                <Button type="primary" onClick={this.createSprint}>Submit</Button>
+                                :
+                                <Button type="primary" onClick={this.updateSprint}>Update</Button>
+                        }
                     </Form.Item>
                 </Form>
 
--- src/views/app-views/project/project-views/project-sprints.js
+++ src/views/app-views/project/project-views/project-sprints.js
@@ -3,8 +3,36 @@ import {withRouter} from "react-router-dom";
 import SprintContainer from "../../../../components/sp-componenets/sprint-components/sprint-container";
 import { PlusOutlined } from '@ant-design/icons';
 import {Button} from "antd";
+import SprintEditModal from "../../../../components/sp-componenets/sprint-components/sprint-edit-modal";
+import * as spinner_actions from "../../../../redux/actions/Spinner";
+import * as navigation_actions from "../../../../redux/actions/Navigation";
+import * as project_actions from "../../../../redux/actions/Project";
+import {connect} from "react-redux";
 
 class ProjectSprints extends React.Component {
+
+    state = {
+        isEditModal: false,
+        modalType: null,
+        sprint: null
+    }
+
+    handleModal = (type, val) => {
+        if(!val) {
+            this.setState({isEditModal: val, modalType: null})
+        } else {
+            this.setState({isEditModal: val, modalType: type})
+        }
+    }
+
+    openSprintCreateModal = () => {
+        this.handleModal('CREATE', true);
+    }
+
+    openSprintUpdateModal = (p) => {
+        this.setState({sprint: p});
+        this.handleModal('UPDATE', true);
+    }
 
     render() {
         return(
@@ -14,13 +42,31 @@ class ProjectSprints extends React.Component {
                     <SprintContainer />
                     <SprintContainer />
                     <div>
-                        <Button type="primary"><PlusOutlined />Create Sprint</Button>
+                        <Button type="primary" onClick={this.openSprintCreateModal}><PlusOutlined />Create Sprint</Button>
                     </div>
                 </div>
+
+                <SprintEditModal type={this.state.modalType} isEditVisible={this.state.isEditModal} onClose={this.handleModal} projectId={this.props.projectReducer.id} />
+
             </div>
         );
     }
 
 }
+const mapStateToProps = (state) => ({
 
-export default withRouter(ProjectSprints);
+    corporateReducer: state.corporateReducer,
+    projectReducer: state.projectReducer
+
+});
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        // corporateHandler: (data) => dispatch(corporate_actions.storeCorporateId(data)),
+        handleSpinner: (data) => dispatch(spinner_actions.handlerSpinner(data)),
+        handleNavigation: (data) => dispatch(navigation_actions.handlerNavigation(data)),
+        handleProjectId: (data) => dispatch(project_actions.handleProjectId(data))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(ProjectSprints));
