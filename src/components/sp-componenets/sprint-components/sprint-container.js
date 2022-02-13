8a26d82ede8f8295042dc2f089421355aed1aa4a
KavinduAkash <bkkasamarasingha1220@gmail.com>
2022-02-13 12:12:25 +0530

    load user stories inside sprints

--- src/components/sp-componenets/sprint-components/sprint-container.js
+++ src/components/sp-componenets/sprint-components/sprint-container.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Button, Col, Dropdown, Menu, Row, Select, Table, Tag, Tooltip} from "antd";
 import './sprint-container.scss'
-import { CaretDownOutlined, EditOutlined, EllipsisOutlined, CheckCircleOutlined, SyncOutlined, ClockCircleOutlined, MenuOutlined, SelectOutlined, AlertOutlined, PlusOutlined, CaretRightOutlined } from '@ant-design/icons';
+import { CaretDownOutlined, MinusCircleOutlined, WarningOutlined, NodeIndexOutlined, ArrowRightOutlined, DeleteOutlined, EyeOutlined, UnorderedListOutlined, EditOutlined, EllipsisOutlined, CheckCircleOutlined, SyncOutlined, ClockCircleOutlined, MenuOutlined, SelectOutlined, AlertOutlined, PlusOutlined, CaretRightOutlined } from '@ant-design/icons';
 import {SortableContainer, SortableElement, SortableHandle} from "react-sortable-hoc";
 import {arrayMoveImmutable} from "array-move";
 import SprintEditModal from "./sprint-edit-modal";
@@ -41,7 +41,7 @@ const columns = [
     {
         title: 'User Story',
         dataIndex: 'user_story',
-        width: '80%',
+        width: '70%',
     },
     {
         title: 'Priority',
@@ -52,6 +52,11 @@ const columns = [
         title: 'Status',
         dataIndex: 'status',
         width: '5%',
+    },
+    {
+        title: 'Sprint',
+        dataIndex: 'sprint',
+        width: '10%',
     },
     {
         title: 'Actions',
@@ -169,6 +174,83 @@ class SprintContainer extends React.Component {
 
 
     render() {
+
+        let dataSource = [];
+        let todo = 0;
+        let process = 0;
+        let done = 0;
+        this.props.sprint.userStory.map((r, i)=>{
+            let isOtherSprints = (r.otherSprints!=null & r.otherSprints!='' & r.otherSprints!=undefined)?r.otherSprints.length>0?true:false:false;
+
+            let menu = (
+                <Menu>
+                    <Menu.Item style={{color:'#1976D2'}}><EyeOutlined /> View</Menu.Item>
+                    <Menu.ItemGroup title="Move to sprint">
+                        {
+                            isOtherSprints?r.otherSprints.map((rx, index)=><Menu.Item key={index} onClick={()=>this.props.move_user_story(r.id, rx.id)}><ArrowRightOutlined />Move to {rx.sprintName}</Menu.Item>):<Tag icon={<MinusCircleOutlined />} color="default">
+                                No Sprints
+                            </Tag>
+                        }
+                    </Menu.ItemGroup>
+                    <Menu.Item style={{color:'#AD1457'}}><DeleteOutlined /> Remove</Menu.Item>
+                </Menu>
+            );
+
+            let obj = {
+                key: i,
+                edit: <Button type={'link'}
+                    // onClick={()=>this.props.openEdit(r, i)}
+                >
+                    <SelectOutlined />
+                </Button>,
+                user_story: r.title,
+                priority:
+                    (r.priority=='HIGH')?
+                        <Tooltip placement="top" title={'Priority High 🤯'}>
+                                <span style={{color: 'red'}}>
+                                    <AlertOutlined />
+                                </span>
+                        </Tooltip>
+                        :(r.priority=='MEDIUM')?
+                        <Tooltip placement="top" title={'Priority Medium 🙂'}>
+                                <span style={{color: 'green'}}>
+                                    <AlertOutlined />
+                                </span>
+                        </Tooltip>
+                        :<Tooltip placement="top" title={'Priority low ☺️'}>
+                                <span style={{color: '#90CAF9'}}>
+                                    <AlertOutlined />
+                                </span>
+                        </Tooltip>
+                ,
+                status:
+                    <Select defaultValue={r.statusType} style={{ width: 120 }}
+                        // onChange={handleChange}
+                    >
+                        <Option value="TODO" style={{backgroundColor: '#CFD8DC'}}>Todo</Option>
+                        <Option value="PROCESSING" style={{backgroundColor: '#B3E5FC'}}>Processing</Option>
+                        <Option value="DONE" style={{backgroundColor: '#C8E6C9'}}>Done</Option>
+                    </Select>
+                ,
+                sprint: <span>{r.sprint!=null?r.sprint.sprintName:<Tooltip placement="top" title={'No assigned sprint yet ⚠️'}><WarningOutlined /></Tooltip>}
+                    <a type={'text'} className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                                <NodeIndexOutlined />
+                            </a>
+                    </span>,
+                actions:
+                    <Dropdown overlay={menu}>
+                        <Button type={'text'} className="ant-dropdown-link" onClick={e => e.preventDefault()}>
+                            <EllipsisOutlined />
+                        </Button>
+                    </Dropdown>,
+                index: 0
+            }
+            dataSource.push(obj);
+            if(r.statusType=='TODO') {todo = todo + 1}
+            if(r.statusType=='PROCESSING') {process = process + 1}
+            if(r.statusType=='DONE') {done = done + 1}
+        })
+
         return (
             <div>
                 <div className={'sprint-container'}>
@@ -181,9 +263,9 @@ class SprintContainer extends React.Component {
                                 <span className={'edit'}><Button type={'text'} onClick={()=>this.props.updateSprint(this.props.sprint)}><EditOutlined />Edit</Button></span>
                             </Col>
                             <Col xs={12} sm={12} md={12} lg={12} xl={12} className={'section2'}>
-                                <span><Tag icon={<ClockCircleOutlined />} color="default">{`${0}`}</Tag></span>
-                                <span><Tag icon={<SyncOutlined />} color="processing">{`${0}`}</Tag></span>
-                                <span><Tag icon={<CheckCircleOutlined />} color="success">{`${0}`}</Tag></span>
+                                <span><Tag icon={<ClockCircleOutlined />} color="default">{`${todo}`}</Tag></span>
+                                <span><Tag icon={<SyncOutlined />} color="processing">{`${process}`}</Tag></span>
+                                <span><Tag icon={<CheckCircleOutlined />} color="success">{`${done}`}</Tag></span>
                                 <span className={'action-1'}><Button type="primary" size={'small'}>Start Sprint</Button></span>
                                 <span className={'action-1'}>
                                <Dropdown overlay={menu}>
@@ -202,7 +284,7 @@ class SprintContainer extends React.Component {
                             <div className={'sprint-container-body'}>
                                 <Table
                                     pagination={false}
-                                    dataSource={this.state.dataSource}
+                                    dataSource={dataSource}
                                     columns={columns}
                                     rowKey="index"
                                     components={{
